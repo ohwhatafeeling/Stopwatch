821c59d0bd01277598e6c6f4851bf777f9747967
Michael Macdonald <michaelmacdonald@Michaels-Air.modem>
2022-06-28 11:37:55 +0930

    UI done

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,4 +1,55 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class MyFrame extends JFrame implements ActionListener {
+	JLabel time;
+	JButton startStop;
+	JButton reset;
+	
+	MyFrame() {
+		time = new JLabel();
+		time.setBounds(25, 25, 250, 50);
+		time.setBackground(Color.BLACK);
+		time.setOpaque(true);
+		time.setText("00:00.000");
+		time.setFont(new Font(null, Font.BOLD, 45));
+		time.setForeground(Color.GREEN);
+		time.setHorizontalAlignment(JLabel.CENTER);
+
+		reset = new JButton();
+		reset.setBounds(40, 90, 100, 50);
+		reset.setText("Reset");
+		
+		startStop = new JButton();
+		startStop.setBounds(160, 90, 100, 50);
+		startStop.setText("Start");
+	
+		this.setTitle("Mick's Stopwatch");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(300, 200);
+		this.setResizable(false);
+		this.setLayout(null);
+		this.setLocationRelativeTo(null);
+		this.getContentPane().setBackground(new Color(0x90fbfc));
+		
+		this.add(time);
+		this.add(reset);
+		this.add(startStop);
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
